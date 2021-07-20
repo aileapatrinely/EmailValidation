f1d43dd7382c3f9571662a7369b1541f59b8eb5c
Ailea Patrinely <aileapatrinely@Aileas-MacBook-Air.local>
2021-07-20 13:41:32 -0500

    updated format for success.php

--- success.php
+++ success.php
@@ -15,6 +15,8 @@ date_default_timezone_set('America/Chicago');
     <h1>It is a valid email address!</h1>
     <p>Your Email: <?= $_SESSION['email'] ?></p>
     <!-- need to show the date  -->
+    <h3>Previously Entered Email Addresses:</h3>
+    <!--some php with a for loop to show emails and date created--!>
     <!-- maybe figure out a delete button/fx -->
 </body>
 </html>

22f618a85d006411c5955fbe2f9442dce417203d
Ailea Patrinely <aileapatrinely@Aileas-MacBook-Air.local>
2021-07-20 14:28:12 -0500

    structure for if/else statements added to process.php

--- process.php
+++ process.php
@@ -8,11 +8,33 @@ $_SESSION['error'] = false;
 //if statements checking post
 if(!empty($_POST) && isset($_POST['email'])){
     //if statement checking filter_validate_email
+    if(!filter_var($_POST['email'], filter_validate_email)){
         //set session error
+        $_SESSION['error']=true;
         //header location
+        header('location: index.php');
         //die
+        die();
+    }else{
+        //email passed validations so we save it
+        //query statement for inserting into database
+        //move it to success and kill it
+        if($last_row_id){
+            //something about last row id?
+        }else{
+        //save fails die
+        }
+    }
+}else if(!empty($_POST) && isset($_POST['action'])){
+    if($_POST['action'] == 'delete'){
+        //this is our delete query
+        //move to success and kill it
+    }else{
+        //die
+        die('delete form submitted but action is not equal to delete');
+    }
 }else{
-    //email passed validations so we save it
-    //query statement for inserting into database
+    header('location: index.php');
+    die();
 }
 ?>
